8ba553a3c57b4d2552ad13ad7f2fdb0ae8e833d8
ChouKevin <uuuu12tw@gmail.com>
2024-04-15 15:34:16 +0800

    add util: graph, interval

---
+++ src/main/java/com/leetcode/util/Interval.java
@@ -0,0 +1,9 @@
+package com.leetcode.util;
+
+public class Interval {
+    public int start, end;
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
--- src/main/java/com/leetcode/util/Node.java
+++ src/main/java/com/leetcode/util/Node.java
@@ -1,13 +1,22 @@
 package com.leetcode.util;
 
+import java.util.ArrayList;
+
 public class Node {
+    // for link list
     public int val;
     public Node next;
     public Node random;
-
     public Node(int val) {
         this.val = val;
         this.next = null;
         this.random = null;
+    }
+
+    // for graph
+    public ArrayList<Node> neighbors;
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
     }
 }
